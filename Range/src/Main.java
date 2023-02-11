73d9e0bcd12aefa37fc6b238b1a04f5505b60814
Tatyana <jahnke@rambler.ru>
2023-02-11 19:58:15 +0700

    Hello world

--- Range/src/Main.java
+++ Range/src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Range range = new Range(1, 20);
+
+        System.out.println("Начало диапазона: " + range.getFrom());
+        System.out.println("Конец диапазона: " + range.getTo());
+        System.out.println();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите число:");
+        double dot = scanner.nextDouble();
+
+        System.out.println(range.isInside(dot));
+        System.out.println();
+
+        System.out.print("Длина диапазона: от " + range.getFrom() + " до " + range.getTo());
+
+        System.out.println();
+
+        range.setFrom(12);
+        range.setTo(36);
+        System.out.println(range.getFrom());
+        System.out.println(range.getTo());
+
+        System.out.println("Длина диапазона: " + range.getLength());
     }
 }
